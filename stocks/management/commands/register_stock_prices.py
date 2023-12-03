dc6ffdce4c864a65d8d50956254fad2d6dc57e2f
Raynor <jonggu.woo@fitpet.co.kr>
2023-12-03 18:37:44 +0900

    Refactor stock price registration command
    The process for registering stock prices has been updated for more efficient batch processing and better logging. Import statements have been reorganised, logger has been added to the command class, and a comment for the --stocks argument has been included for improved clarity. The stock price generation has been optimized and the batch_size of the bulk create function has been increased to 1000 to improve performance. Info logging has been added after each bulk creation of stock prices to provide more visibility about the number of new prices created in each run.

--- stocks/management/commands/register_stock_prices.py
+++ stocks/management/commands/register_stock_prices.py
@@ -5,13 +5,19 @@ from decimal import Decimal
 import FinanceDataReader as fdr
 from django.core.management import BaseCommand
 
-from mystock.core.utils import check_nan_return_or_zero
+from mystock.core.constants import MARKET_TYPE_STOCK
+from mystock.core.utils import check_nan_return_or_zero, logger
 from stocks.models import Stock, StockPrice
+
+log = logger
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--stocks", nargs="+", type=str, required=True)
+        """
+        --stocks 쌍용C&E 롯데지주 현대해상 LS CJ
+        """
 
     def handle(self, *args, **options):
         stocks = Stock.objects.filter(name__in=options.get("stocks", []))
@@ -25,19 +31,17 @@ class Command(BaseCommand):
             else:
                 dataset = fdr.DataReader(stock.code, last_stock_price.next_update_date)
 
-            new_stock_prices.extend(
-                [
-                    StockPrice(
-                        date=datetime.strptime(date.strftime("%y-%m-%d"), "%y-%m-%d"),
-                        price_open=Decimal(row["Open"]),
-                        price_close=Decimal(row["Close"]),
-                        price_high=Decimal(row["High"]),
-                        price_low=Decimal(row["Low"]),
-                        price_change=check_nan_return_or_zero(row["Change"]),
-                        stock=stock,
-                    )
-                    for date, row in dataset.iterrows()
-                ]
-            )
-
-        StockPrice.objects.bulk_create(new_stock_prices, batch_size=500)
+            new_stock_prices = [
+                StockPrice(
+                    date=datetime.strptime(date.strftime("%y-%m-%d"), "%y-%m-%d"),
+                    price_open=Decimal(row["Open"]),
+                    price_close=Decimal(row["Close"]),
+                    price_high=Decimal(row["High"]),
+                    price_low=Decimal(row["Low"]),
+                    price_change=check_nan_return_or_zero(row["Change"]),
+                    stock=stock,
+                )
+                for date, row in dataset.iterrows()
+            ]
+            StockPrice.objects.bulk_create(new_stock_prices, batch_size=1000)
+            log.info(f"{stock.name} prices created, {len(new_stock_prices)}")
